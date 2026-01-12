774a4147bd0281e45a36ee3aed4ed485fbe855ca
stkevintan <stkevintan@zju.edu.cn>
2026-01-12 22:29:21 +0800

    fix comments

--- web/App.svelte
+++ web/App.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import Router from 'svelte-spa-router';
-  import Login from './routes/Login.svelte';
-  import Dashboard from './routes/Dashboard.svelte';
+    import Router from 'svelte-spa-router';
+    import Login from './routes/Login.svelte';
+    import Dashboard from './routes/Dashboard.svelte';
 
-  const routes = {
-    '/': Login,
-    '/login': Login,
-    '/dashboard': Dashboard,
-    '*': Login,
-  };
+    const routes = {
+        '/': Login,
+        '/login': Login,
+        '/dashboard': Dashboard,
+        '*': Login,
+    };
 </script>
 
 <Router {routes} />
 
 <style lang="scss">
-  /* SCSS power here */
+    /* SCSS power here */
 </style>
