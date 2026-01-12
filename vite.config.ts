f523296cd5a81eaf03d2967433c88d422d2896f7
stkevintan <stkevintan@zju.edu.cn>
2026-01-12 22:23:06 +0800

    use env files

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    svelte(),
-    tailwindcss(),
-  ],
-  server: {
-    port: 8081,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3334',
-        changeOrigin: true,
-      },
-      '/rest': {
-        target: 'http://localhost:3334',
-        changeOrigin: true,
-      },
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './web'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const port = parseInt(env.PORT || '8081')
+
+  return {
+    plugins: [
+      svelte(),
+      tailwindcss(),
+    ],
+    server: {
+      port: port === 8081 ? 8082 : 8081,
+      proxy: {
+        '/api': {
+          target: `http://localhost:${port}`,
+          changeOrigin: true,
+        },
+        '/rest': {
+          target: `http://localhost:${port}`,
+          changeOrigin: true,
+        },
+      }
     },
-  },
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './web'),
+      },
+    },
+    build: {
+      outDir: './dist',
+      emptyOutDir: true,
+    },
+  }
 })
