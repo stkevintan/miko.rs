ae5ad1556da3505ab2ff9b26f74367477b2294f8
Kevin Tan <stkevintan@zju.edu.cn>
2026-01-12 17:45:51 +0800

    init basic web (#30)

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './web'),
+    },
+  },
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+  },
+})
