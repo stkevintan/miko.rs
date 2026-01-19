4b68e5beda5b2aff0ff7f41b0802a8da48068a78
Kevin Tan <stkevintan@zju.edu.cn>
2026-01-19 10:27:09 +0800

    implement change password (#53)
    * implement change password
    
    * show security tip

--- src/subsonic/mod.rs
+++ src/subsonic/mod.rs
@@ -83,7 +83,7 @@ fn base_routes() -> Route {
         ("/createUser", user::create_user),
         ("/updateUser", user::update_user),
         ("/deleteUser", user::delete_user),
-        ("/changePassword", shared::not_implemented),
+        ("/changePassword", shared::not_supported),
         // list
         ("/getAlbumList", lists::get_album_list),
         ("/getAlbumList2", lists::get_album_list2),
